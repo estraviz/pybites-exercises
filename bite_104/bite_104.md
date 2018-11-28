5f0c7183e4f03c848cd9d53ee4712337a9b3c347
estraviz <javier.estraviz@gmail.com>
2018-11-29 00:41:23 +0100

    Added bite_104

---
+++ bite_104/bite_104.md
@@ -0,0 +1,7 @@
+# Bite 104. Split and join
+
+Split up the message on newline (\n) using the `split` builtin, then use the `join` builtin to stitch it together using a '|' (pipe).
+
+So `Hello world:\nI am coding in Python :)\nHow awesome!` would turn into: `Hello world:|I am coding in Python :)|How awesome!`
+
+Your code should work for other message strings as well.
---
+++ bite_104/pipe.py
@@ -0,0 +1,15 @@
+'''
+Bite 104. Split and join
+'''
+
+message = """Hello world!
+We hope that you are learning a lot of Python.
+Have fun with our Bites of Py.
+Keep calm and code in Python!
+Become a PyBites ninja!"""
+
+
+def split_in_columns(message=message):
+    """Split the message by newline (\n) and join it together on '|'
+       (pipe), return the obtained output"""
+    return '|'.join(map(str, message.split('\n')))
