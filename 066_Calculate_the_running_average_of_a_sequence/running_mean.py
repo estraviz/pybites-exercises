44ca639545b771bf19abf1eff5e5ceaeb8b82a2d
estraviz <javier.estraviz@gmail.com>
2020-06-03 00:04:09 +0200

    Refactor that uses itertools.accumulate

--- 066_Calculate_the_running_average_of_a_sequence/running_mean.py
+++ 066_Calculate_the_running_average_of_a_sequence/running_mean.py
@@ -9,7 +9,5 @@ def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    output = []
-    for i, _ in enumerate(accumulate(sequence)):
-        output.append(round(sum(sequence[:i+1])/(i+1), 2))
-    return output
+    for i, num in enumerate(accumulate(sequence)):
+        yield round(num/(i + 1), 2)
